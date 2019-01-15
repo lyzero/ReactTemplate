2a75b149a93bd389ce30c4dab1fc6d1ab56f5e8a
Ying Lin <linyin@amazon.com>
2019-01-14 16:51:10 -0800

    Added trip creation endpoint

--- AwesomeProject/components/Constants/Endpoints.js
+++ AwesomeProject/components/Constants/Endpoints.js
@@ -1,4 +1,6 @@
 export const travelBackendEndpoint = "http://localhost:8080/"
 
-export const createUserTarget = "user/create"
+export const createUserTarget = "user/"
 export const getUserTarget = "user/"
+
+export const createTripTarget = "trip/"
